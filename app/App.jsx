5221b5df7a4eff1131d8cc704f8f5d566b374bbd
田想兵 <tianxb@shinemo.com>
2016-05-25 18:31:52 +0800

    'h'

--- app/App.jsx
+++ app/App.jsx
@@ -4,10 +4,12 @@ import { Router, Route, IndexRoute, Link, IndexLink, hashHistory } from 'react-r
 
 import App from './page/App/App';
 import Record from './page/Record/Record';
+import Fieldsign from './page/Fieldsign/Fieldsign';
 
 render((
   <Router history={hashHistory}>
     <Route path="/" component={App}/>
     <Route path="/record" component={Record}/>
+    <Route path="/fieldsign" component={Fieldsign}/>
   </Router>
 ), document.getElementById('app'))
---
+++ app/Page/Fieldsign/Fieldsign.jsx
@@ -0,0 +1,11 @@
+import React  from 'react';
+let {Component}=React;
+export default class Fieldsign extends Component{
+	render(){
+		return (
+			<div>
+				外勤签到
+			</div>
+			)
+	}
+}
