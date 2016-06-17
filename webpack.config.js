afef8768de0a35f19d21038251246befd0e4ecf7
田想兵 <tianxb@shinemo.com>
2016-06-17 14:36:18 +0800

    'static'

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require("path");
 var webpack = require('webpack');
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 console.log("+++++++++++" + process.env.NODE_ENV + "***********")
-var TEST = process.env.NODE_ENV == "test"||process.env.NODE_ENV == "dev";
+var TEST = process.env.NODE_ENV == "test" || process.env.NODE_ENV == "dev";
 console.log(TEST)
 var filename = TEST ? "[name]" : "[chunkhash:8].[name]";
 console.log(filename)
@@ -16,9 +16,9 @@ var htmlPlugin = new HtmlWebpackPlugin({
     filename: '../index.html',
     template: "template.html"
 });
-var modulesDirectories = ["web_modules", "node_modules", "bower_components","app/devconfig","app/cyconfig"];
-if(process.env.NODE_ENV =="test"){
-     modulesDirectories = ["web_modules", "node_modules", "bower_components","app/config","app/cyconfig"];
+var modulesDirectories = ["web_modules", "node_modules", "bower_components", "app/devconfig", "app/cyconfig"];
+if (process.env.NODE_ENV == "test") {
+    modulesDirectories = ["web_modules", "node_modules", "bower_components", "app/config", "app/cyconfig"];
 }
 var config = {
     entry: {
@@ -72,13 +72,17 @@ var config = {
         new webpack.optimize.CommonsChunkPlugin("vendor", "base.js"), htmlPlugin
     ]
 };
-if (process.env.NODE_ENV == "test"||process.env.NODE_ENV == "dev") {
+if (process.env.NODE_ENV == "test" || process.env.NODE_ENV == "dev") {
     config.devtool = "source-map";
     config.output.publicPath = "/";
 }
-if(process.env.NODE_ENV =="production"){
-    config.resolve.modulesDirectories = ["web_modules", "node_modules", "bower_components","app/devconfig","app/msconfig"];
-    config.output.path=path.resolve(__dirname, "masheng/build")
+if (process.env.NODE_ENV == "production") {
+    config.resolve.modulesDirectories = ["web_modules", "node_modules", "bower_components", "app/devconfig", "app/msconfig"];
+    config.output.path = path.resolve(__dirname, "masheng/build")
+}
+if (process.env.NODE_ENV == "static") {
+    config.resolve.modulesDirectories = ["web_modules", "node_modules", "bower_components", "app/config", "app/cyconfig"];
+    config.output.path = path.resolve(__dirname, "static/build")
 }
 console.log(config)
 module.exports = config
